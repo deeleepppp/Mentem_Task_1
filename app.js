b884306c2ccecb0085d806fd7c55545066bc8b8f
deeleepppp <kr.dilipsinghh@gmail.com>
2025-03-24 15:50:07 +0530

    add complted option

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 let taskInput = document.querySelector('input');
 let taskList = document.querySelector("ul");
 
-let todos = JSON.parse(localStorage.getItem('todos'));
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 function renderTasks() {
-   
     taskList.innerHTML = "";
 
-    
     todos.forEach((task, index) => {
         let li = document.createElement("li");
-
+        
         let editBtn = document.createElement("img");
-        editBtn.src = 'https://media.istockphoto.com/id/1309236787/photo/3d-simple-pencil-with-docunent-icon-on-pastel-background-hight-quality-3d-illustration-edit.jpg?s=1024x1024&w=is&k=20&c=GqI3hnvKcGI_qUio9ijXVloXVXmJzfpfSuEjzFXmRq8='
+        editBtn.src = 'https://media.istockphoto.com/id/1309236787/photo/3d-simple-pencil-with-docunent-icon-on-pastel-background-hight-quality-3d-illustration-edit.jpg?s=1024x1024&w=is&k=20&c=GqI3hnvKcGI_qUio9ijXVloXVXmJzfpfSuEjzFXmRq8=';
         li.appendChild(editBtn);
 
         let delBtn = document.createElement("button");
         delBtn.textContent = "X";
         li.appendChild(delBtn);
 
+        let completeBtn = document.createElement("i");
+        completeBtn.textContent = '✓';
+        li.appendChild(completeBtn);
+        
         let taskSpan = document.createElement("span");
         taskSpan.textContent = task.text;
         li.appendChild(taskSpan);
+        
+        if (task.completed) {
+            taskSpan.style.textDecoration = "line-through";
+            taskSpan.style.color = "gray";
+        }
 
         delBtn.onclick = function () {
             todos.splice(index, 1); 
@@ -33,7 +40,6 @@ function renderTasks() {
             let input = document.createElement("input");
             input.type = "text";
             input.value = task.text;
-
             li.replaceChild(input, taskSpan);
             input.focus();
 
@@ -46,6 +52,12 @@ function renderTasks() {
             });
         };
 
+        completeBtn.onclick = function () {
+            task.completed = !task.completed;
+            updateLocalStorage();
+            renderTasks();
+        };
+
         taskList.appendChild(li);
     });
 }
@@ -54,19 +66,15 @@ function addTask() {
     let taskText = taskInput.value;
     if (taskText === "") return;
 
-    todos.push({ text: taskText });
-
+    todos.push({ text: taskText, completed: false });
     updateLocalStorage();
     renderTasks();
-
-    taskInput.value = ""; 
+    taskInput.value = "";
 }
-
 
 function updateLocalStorage() {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
-
 
 taskInput.addEventListener("keypress", function (event) {
     if (event.key === 'Enter') {
@@ -74,6 +82,5 @@ taskInput.addEventListener("keypress", function (event) {
         addTask();
     }
 });
-
 
 renderTasks();
